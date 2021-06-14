10284346167b5ed9553b2075b7ceca56b42c8e40
Huy <mac@HuyPhuongs-Macbook.local>
2021-06-15 02:55:30 +0700

    fix/socketurl

--- src/server-interaction/socket.services.ts
+++ src/server-interaction/socket.services.ts
@@ -1,17 +1,7 @@
 import { io, Socket } from "socket.io-client";
 
-const {
-  NODE_ENV,
-  REACT_APP_SOCKET_URL_DEV,
-  REACT_APP_SOCKET_URL_BUILD,
-  REACT_APP_SOCKET_URL_TEST,
-} = process.env;
-const ioURL =
-  NODE_ENV === "development"
-    ? REACT_APP_SOCKET_URL_DEV
-    : NODE_ENV === "production"
-    ? REACT_APP_SOCKET_URL_BUILD
-    : REACT_APP_SOCKET_URL_TEST;
+
+const ioURL = process.env.REACT_APP_SOCKET_URL;
 
 export const createSocket = () => io(ioURL as string);
 
