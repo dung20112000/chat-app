79f1ccdef8be114167e8e4c561b8fdba04b960a6
Huy <mac@HuyPhuongs-Macbook.local>
2021-06-22 16:45:25 +0700

    fix/loading

--- src/server-interaction/apis/api.services.ts
+++ src/server-interaction/apis/api.services.ts
@@ -1,32 +1,35 @@
 import axios, {AxiosRequestConfig, Method, AxiosInstance} from "axios";
-import {notRequiredTokenApis} from "./apis-list";
+import {notRequiredTokenApis, noLoadingApis} from "./apis-list";
 import {
     checkTokenInterceptors,
     offLoadingInterceptors,
     onLoadingInterceptors
 } from "./interceptors.services";
+import {IMethodUrlObject} from "../../@types/ApiServices";
 
 const baseURL = process.env.REACT_APP_BASE_URL;
 const axiosInstance: AxiosInstance = axios.create({
     baseURL,
 });
-onLoadingInterceptors(axiosInstance);
-offLoadingInterceptors(axiosInstance);
+const isApiMatch = (url: string, method: Method, testArray: IMethodUrlObject[]) => {
+    return testArray.findIndex((api) => api.url === url && api.method.toLowerCase() === method.toLowerCase());
+}
 export const callApi = (
     url: string,
     method: Method,
     body?: any,
     requestConfig?: AxiosRequestConfig
 ) => {
-    const checkRequiredToken = notRequiredTokenApis.findIndex(
-        (api) =>
-            api.url === url && api.method.toLowerCase() === method.toLowerCase()
-    );
-
 
     let tokenInterceptors: number | undefined;
-    if (checkRequiredToken === -1) {
+    let tokenOnLoading:number | undefined;
+    let tokenOffLoading:number | undefined;
+    if (isApiMatch(url, method,notRequiredTokenApis) < 0) {
         tokenInterceptors = checkTokenInterceptors(axiosInstance);
+    }
+    if(isApiMatch(url, method, noLoadingApis) < 0 && !url.includes("/conversations/")){
+        tokenOnLoading = onLoadingInterceptors(axiosInstance);
+        tokenOffLoading = offLoadingInterceptors(axiosInstance);
     }
     return axiosInstance
         .request({
@@ -38,6 +41,12 @@ export const callApi = (
         .finally(() => {
             if (typeof tokenInterceptors === "number") {
                 axiosInstance.interceptors.request.eject(tokenInterceptors);
+            }
+            if (typeof tokenOnLoading === "number"){
+                axiosInstance.interceptors.request.eject(tokenOnLoading);
+            }
+            if (typeof tokenOffLoading === "number"){
+                axiosInstance.interceptors.request.eject(tokenOffLoading);
             }
         });
 };
