d508c9be404ab52b9b806d95dfe58226d0920756
Cong Dung <dungbcph07574@fpt.edu.vn>
2021-06-25 17:09:52 +0700

    socket-file-optimize

--- src/pages/chat-page/ChatPage.tsx
+++ src/pages/chat-page/ChatPage.tsx
@@ -1,15 +1,15 @@
 import AppSideBarCommon from "../../common-components/app-side-bar.common";
 import chatPageRoutes from "../../routes/chat-page.routes";
-import {Switch, Route, useLocation, Redirect, useHistory} from "react-router-dom";
-import {useSelector, useDispatch} from "react-redux";
-import {RootState} from "../../redux/reducers/RootReducer.reducer.redux";
-import {useEffect, useState} from "react";
-import {createSocket, onLogout} from "../../server-interaction/socket-handle/socket.services";
-import {addSocket} from "../../redux/actions/socket.actions.redux";
-import {fetchUserInfos} from "../../redux/actions/users.actions.redux";
-import {notifyNewFriendRequest, notifySuccess} from "../../helpers/functions/notify.helper";
-import {IUserInfosReducer} from "../../@types/redux";
-import {emitClientConnect} from "../../server-interaction/socket-handle/socket.services";
+import { Switch, Route, useLocation, Redirect, useHistory } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "../../redux/reducers/RootReducer.reducer.redux";
+import { useEffect, useState } from "react";
+import { createSocket, onLogout } from "../../server-interaction/socket-handle/socket.services";
+import { addSocket } from "../../redux/actions/socket.actions.redux";
+import { fetchUserInfos } from "../../redux/actions/users.actions.redux";
+import { notifyNewFriendRequest, notifySuccess } from "../../helpers/functions/notify.helper";
+import { IUserInfosReducer } from "../../@types/redux";
+import { emitClientConnect } from "../../server-interaction/socket-handle/socket.services";
 import {
     emitAcceptFriendsRequests,
     onAcceptInfosToSender,
@@ -24,24 +24,24 @@ import {
     fetchFriendRequest,
     removeFriendsRequest
 } from "../../redux/actions/FriendRequest.action.redux";
-import {onStatusToOnlineFriends} from "../../server-interaction/socket-handle/socket-change-status";
-import {updateFriendStatus} from './../../redux/actions/FriendList.actions.redux';
-import {onCreateConversations} from "../../server-interaction/socket-handle/socket-chat";
-import {Socket} from "socket.io-client";
+import { onStatusToOnlineFriends } from "../../server-interaction/socket-handle/socket-change-status";
+import { updateFriendStatus } from './../../redux/actions/FriendList.actions.redux';
+import { onCreateConversations } from "../../server-interaction/socket-handle/socket-chat";
+import { Socket } from "socket.io-client";
 
-import {onComingCall} from "../../server-interaction/socket-handle/socket-peer.services";
-import {openChatWindow} from "../../server-interaction/peerjs/peer.services";
+import { onComingCall } from "../../server-interaction/socket-handle/socket-peer.services";
+import { openChatWindow } from "../../server-interaction/peerjs/peer.services";
 import qs from "querystring";
-import {Button, Modal} from "react-bootstrap";
+import { Button, Modal } from "react-bootstrap";
 import ComingCallModalCommon from "../../common-components/coming-call-modal.common";
-
+import "../../server-interaction/socket-handle/socket-running";
 const ChatPage = () => {
-    const [comingCall,setComingCall] = useState<any>(null);
-    const openModalComing = (callerInfos:any) =>{
+    const [comingCall, setComingCall] = useState<any>(null);
+    const openModalComing = (callerInfos: any) => {
         setComingCall(callerInfos)
     }
-    const closeModalComing = ()=> setComingCall(null);
-    const {pathname} = useLocation();
+    const closeModalComing = () => setComingCall(null);
+    const { pathname } = useLocation();
     const history = useHistory();
     const userInfosStateRedux: IUserInfosReducer = useSelector((state: RootState) => {
         return state.userInfos;
@@ -66,71 +66,38 @@ const ChatPage = () => {
     }, [dispatch, userInfosStateRedux?._id]);
 
     useEffect(() => {
-        if (socketStateRedux  && userInfosStateRedux && userInfosStateRedux._id) {
+        if (socketStateRedux && userInfosStateRedux && userInfosStateRedux._id) {
             emitClientConnect(socketStateRedux, userInfosStateRedux._id);
-            onComingFriendsRequests(socketStateRedux, ({
-                                                           senderFullName,
-                                                           senderId,
-                                                           avatarUrl
-                                                       }: { senderFullName: string, senderId: string, avatarUrl: string }) => {
-                dispatch(fetchFriendRequest());
-                notifyNewFriendRequest({
-                    senderFullName,
-                    senderId,
-                    avatarUrl
-                }, (isAcceptedId: string) => {
-                    const body = {acceptorId: userInfosStateRedux._id, isAcceptedId}
-                    emitAcceptFriendsRequests(socketStateRedux, body, (response: any) => {
-                        response.newFriend.conversationsId = null;
-                        if (response.status) dispatch(acceptFriendRequest(response.newFriend));
-                        dispatch(removeFriendsRequest(isAcceptedId));
-                    })
-                })
-            })
-            onAcceptInfosToSender(socketStateRedux, (response: any) => {
-                if (response) {
-                    response.acceptorInfos.conversationsId = null;
-                    dispatch(acceptFriendRequest(response.acceptorInfos));
-                }
-            })
-            onStatusToOnlineFriends(socketStateRedux, (response: any) => {
-                if (response) dispatch(updateFriendStatus(response));
-            })
-
             onLogout(socketStateRedux, () => {
                 socketStateRedux.disconnect();
-                dispatch({type: "USER_LOGOUT"});
+                dispatch({ type: "USER_LOGOUT" });
                 localStorage.removeItem("authToken");
                 history.push("/");
             })
-            onCreateConversations(socketStateRedux, (response: any) => {
-                if (response.conversationsId) dispatch(updateConversationIdOfFriends(response));
-            })
-            onComingCall(socketStateRedux,(callerInfos:any)=>{
-
+            onComingCall(socketStateRedux, (callerInfos: any) => {
                 openModalComing(callerInfos.callerInfos);
             })
         }
     }, [dispatch, history, socketStateRedux, userInfosStateRedux?._id])
     const chatPageRoutesJSX = chatPageRoutes && chatPageRoutes.length > 0 ? (
         chatPageRoutes.map((route, index) => {
-            const {main, ...rest} = route;
-            return <Route key={index} {...rest} render={() => main()}/>
+            const { main, ...rest } = route;
+            return <Route key={index} {...rest} render={() => main()} />
         })
     ) : null;
 
     if (pathname === "/chat-page") {
-        return <Redirect to={"/chat-page/conversations"}/>
+        return <Redirect to={"/chat-page/conversations"} />
     }
     return (
         <div>
-            <AppSideBarCommon/>
+            <AppSideBarCommon />
             <div className="vh-100 pt-3 overflow-hidden">
                 <Switch>
                     {chatPageRoutesJSX}
                 </Switch>
             </div>
-            <ComingCallModalCommon callerInfos={comingCall} closeModalComing={closeModalComing}/>
+            <ComingCallModalCommon callerInfos={comingCall} closeModalComing={closeModalComing} />
         </div>
     )
 }
--- src/server-interaction/socket-handle/socket-running.ts
+++ src/server-interaction/socket-handle/socket-running.ts
@@ -1,6 +1,120 @@
+import { Socket } from "socket.io-client";
+import { notifyNewFriendRequest } from "../../helpers/functions/notify.helper";
+import {
+  acceptFriendRequest,
+  updateConversationIdOfFriends,
+  updateFriendStatus,
+} from "../../redux/actions/FriendList.actions.redux";
+import {
+  fetchFriendRequest,
+  removeFriendsRequest,
+} from "../../redux/actions/FriendRequest.action.redux";
 import store from "../../redux/store";
+import { onStatusToOnlineFriends } from "./socket-change-status";
+import { onCreateConversations } from "./socket-chat";
+import {
+  emitAcceptFriendsRequests,
+  onAcceptInfosToSender,
+  onComingFriendsRequests,
+} from "./socket-friends-requests";
+import { emitClientConnect } from "./socket.services";
 
-const socketRedux = store.getState().socket;
+let socket: Socket;
+let friendsList: any;
+let userInfos: any;
+let friendsRequests: any;
+let conversationDetail: any;
 
+function friendsRequestsRedux() {
+  if (socket && userInfos && userInfos._id) {
+    onComingFriendsRequests(
+      socket,
+      ({
+        senderFullName,
+        senderId,
+        avatarUrl,
+      }: {
+        senderFullName: string;
+        senderId: string;
+        avatarUrl: string;
+      }) => {
+        store.dispatch(fetchFriendRequest());
+        notifyNewFriendRequest(
+          {
+            senderFullName,
+            senderId,
+            avatarUrl,
+          },
+          (isAcceptedId: string) => {
+            const body = { acceptorId: userInfos._id, isAcceptedId };
+            emitAcceptFriendsRequests(socket, body, (response: any) => {
+              response.newFriend.conversationsId = null;
+              if (response.status)
+                store.dispatch(acceptFriendRequest(response.newFriend));
+              store.dispatch(removeFriendsRequest(isAcceptedId));
+            });
+          }
+        );
+      }
+    );
+  }
+}
 
+function friendsListRedux() {
+  if (socket && userInfos && userInfos._id) {
+    console.log(socket);
+    onAcceptInfosToSender(socket, (response: any) => {
+      if (response) {
+        response.acceptorInfos.conversationsId = null;
+        store.dispatch(acceptFriendRequest(response.acceptorInfos));
+      }
+    });
 
+    onStatusToOnlineFriends(socket, (response: any) => {
+      console.log(response);
+      if (response) store.dispatch(updateFriendStatus(response));
+    });
+  }
+}
+
+function conversationRedux() {
+  if (socket && userInfos && userInfos._id) {
+    onCreateConversations(socket, (response: any) => {
+      if (response.conversationsId)
+        store.dispatch(updateConversationIdOfFriends(response));
+    });
+  }
+}
+
+store.subscribe(() => {
+  let socketPreviousValue: any = socket;
+  let friendsListPreviousValue = friendsList;
+  let userInfosPreviousValue = userInfos;
+  let friendsRequestsPreviousValue = friendsRequests;
+  let conversationDetailPreviousValue = conversationDetail;
+
+  socket = store.getState().socket;
+  friendsList = store.getState().friendsList;
+  userInfos = store.getState().userInfos;
+  friendsRequests = store.getState().friendsRequests;
+  conversationDetail = store.getState().conversationDetail;
+  //   if (
+  //     JSON.stringify(socket) === JSON.stringify(socketPreviousValue) ||
+  //     JSON.stringify(friendsList) !== JSON.stringify(friendsListPreviousValue) ||
+  //     JSON.stringify(userInfos) !== JSON.stringify(userInfosPreviousValue) ||
+  //     JSON.stringify(friendsRequests) !==
+  //       JSON.stringify(friendsRequestsPreviousValue) ||
+  //     JSON.stringify(conversationDetail) !==
+  //       JSON.stringify(conversationDetailPreviousValue)
+  //   ) {
+  //   }
+
+  if (
+    socket !== socketPreviousValue ||
+    JSON.stringify(userInfos) !== JSON.stringify(userInfosPreviousValue)
+  ) {
+    friendsRequestsRedux();
+    friendsListRedux();
+    conversationRedux();
+  }
+});
